679529fd87e1a50ae48f3ce0d707fc4edd41756c
DebiKk-tech <mtaldin123@gmail.com>
2022-04-20 21:41:19 +0300

    added balance and add_money command for admin

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+users = {}
+
 
 class Things(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.admin_role_id = False
+        self.admin_role_id = 966040422050857084
 
     @commands.command(name='test_role')
     async def test_role(self, ctx, role):
@@ -23,6 +25,29 @@ class Things(commands.Cog):
         else:
             await ctx.channel.send(f'Увы, команда доступна только для <@&{self.admin_role_id}>')
 
+    @commands.command(name='add_money')
+    async def add_money(self, ctx, amount, target):
+        author_roles = list(map(lambda x: x.id, ctx.author.roles))  # Получаю список айдишников ролей автора сообщения
+        print(author_roles)
+        if self.admin_role_id in author_roles or not self.admin_role_id:
+            print(target)
+            target = int(target.lstrip('<@').rstrip('>'))
+            if target in users:
+                users[target] += int(amount)
+            else:
+                users[target] = int(amount)
+            await ctx.channel.send(f'Добавлено {amount} рублей пользователю <@{target}> \n Теперь у него {users[target]} рублей')
+        else:
+            await ctx.channel.send(f'Увы, команда доступна только для <@&{self.admin_role_id}>')
+
+    @commands.command(name='balance')
+    async def balance(self, ctx):
+        id_need_to_check = int(ctx.author.id)
+        if id_need_to_check in users:
+            await ctx.channel.send(f'{ctx.author.mention}, ваш баланс составляет {users[id_need_to_check]} рублей')
+        else:
+            await ctx.channel.send('Перезайди на сервер, друже, тебя нет в базе данных')
+
 
 @bot.event
 async def on_message(message):
@@ -30,6 +55,13 @@ async def on_message(message):
     await bot.process_commands(message)
 
 
+@bot.event
+async def on_member_join(member):
+    users[member.id] = 100
+    await member.create_dm()
+    await member.dm_channel.send(f'Привет, {member.name}! Добро пожаловать на сервер! Я начислил тебе 100 рублей, теперь они у тебя на счету!')
+
+
 bot.add_cog(Things(bot))
-TOKEN = "OTY2MzY3OTM2NTcyOTY5MDEw.YmAuRg.wLyCwPRv2Ji7_XP5rOd51yfG_LU"
+TOKEN = "OTY2MzY3OTM2NTcyOTY5MDEw.YmAuRg.nRpgIdkljkqhpZdPcMDAHyiOrjI"
 bot.run(TOKEN)
